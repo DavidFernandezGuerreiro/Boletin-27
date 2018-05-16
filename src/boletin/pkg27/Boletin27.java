d338d24a28775eb8466e7897239948747839d3f5
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-05-16 09:32:58 +0200

    Commit git

---
+++ src/boletin/pkg27/Boletin27.java
@@ -0,0 +1,18 @@
+
+package boletin.pkg27;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora obx=new Calculadora();
+        obx.setVisible(true);
+    }
+    
+}
